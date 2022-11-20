8361797d36cbd6f80edd9ca207b57f916bbd7a68
Fabricio Castillo <46402074+eyrent@users.noreply.github.com>
2022-11-20 18:03:25 -0300

    TG-14: Created login e2e test

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,9 +1,34 @@
 import LoginPage from  '../pageobjects/login.page';
 import InventoryPage from '../pageobjects/inventory.page';
 
+beforeAll(async () => {
+    await LoginPage.open();
+});
+
 describe('Login page', () => {
+
+    it('should not login with some empty login field', async () => {
+        await LoginPage.login('standard_user', '');
+        expect(await LoginPage.getLoginMessage())
+            .toMatch(/password/i);
+        await LoginPage.login('', 'secret_sauce');
+        expect(await LoginPage.getLoginMessage())
+            .toMatch(/username/i);
+    });
+
+    it('should not login with invalid credentials',async () => {
+        await LoginPage.login('unknown ', 'password');
+        expect(await LoginPage.getLoginMessage())
+            .toMatch(/Username and password/i);
+    });
+
+    it('should not login if user is blocked', async () => {
+        await LoginPage.login('locked_out_user', 'secret_sauce');
+        expect(await LoginPage.getLoginMessage())
+            .toMatch(/locked/i);
+    });
+
     it('should login with valid credentials', async () => {
-        await LoginPage.open();
         await LoginPage.login('standard_user', 'secret_sauce');
         await expect(InventoryPage.LogoutLink).toBeExisting();
         await expect(InventoryPage.LogoutLink).toHaveElementProperty('innerText','Logout');
